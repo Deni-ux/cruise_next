534d400f010677521503144971cd1dbadefbcb7d
Denisa Haka <denisa.haka@ymail.com>
2022-07-29 09:46:07 +0100

    about setup

--- components/About.jsx
+++ components/About.jsx
@@ -1,9 +1,51 @@
-﻿import React from 'react'
+﻿import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/About.module.css';
+import cru1 from '../public/img/cru1.jpg'
+import cru2 from '../public/img/cru2.jpg'
+import cru3 from '../public/img/cru3.jpg'
+import cru4 from '../public/img/cru4.jpg'
+
+
 
 const About = () => {
+    
   return (
-    <div><h1>Our reccomandations</h1></div>
+      <div className={styles.about}>
+          <h1>Adventures to have</h1>
+      <div className={styles.grid}>
+              <div className={styles.card}>
+                  <Image src={cru1} alt="cruise" className={styles.img} />
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </div>
+
+              <div className={styles.card}>
+                  <Image src={cru2} alt="cruise" className={styles.img} />
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </div>
+
+          <div
+            className={styles.card}
+              >
+                  <Image src={cru3} alt="cruise" className={styles.img} />
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </div>
+
+          <div
+            className={styles.card}
+              >
+                  <Image src={cru4} alt="cruise" className={styles.img} />
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </div>
+        </div></div>
   )
 }
 
-export default About
+export default About;
+
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className={styles.containerHero}>
           <Image src={cruiseship} alt="sea" className={styles.imgBg} />
           <div className={styles.cards}>
-              <h1 className={styles.bgText}>Book Your <span className={styles.hblue}>Holiday</span>  Now</h1>
+              <h1 className={styles.bgText}> <span className={styles.hblue}>Book</span> Your <span className={styles.hblue}>Holiday</span>  Now</h1>
           <input type='search' name="search-form" placeholder='Start searching..' className={styles.input} />
         </div>
           
