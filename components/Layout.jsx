88612106554362362a2bd5b16f91abdbd145413a
Denisa Haka <denisa.haka@ymail.com>
2022-07-29 06:14:54 +0100

    layout wrap

---
+++ components/Layout.jsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import Head from 'next/head';
+
+
+//layout to wrap website. it has access to props children
+const Layout = ({children}) => {
+  return (
+    <div>
+      <Head>
+        <title>My Kind of Cruise App</title>
+< meta name="description" content="Book you perfect cruise holiday!"></meta>    
+      </Head>
+      
+      
+      {children}
+     
+     
+    </div>
+  )
+}
+
+export default Layout;
