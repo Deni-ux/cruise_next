e75aed0eaa23dd828a5392679196f445f57502c2
Denisa Haka <denisa.haka@ymail.com>
2022-08-02 11:58:08 +0100

    example of new path for each route

---
+++ pages/cruises/[name].js
@@ -0,0 +1,41 @@
+﻿import { data } from "../../data";
+
+const Cruises = ({cruise}) => {
+  return (
+      <div>
+          {cruise.map(cruise => (
+              <div key={cruise.id}>{cruise.name}</div>
+          ))}
+    </div>
+  )
+}
+
+
+//create a new path for each new route
+export const getStaticPaths = async () => {
+    const cruises = data;
+    const paths = cruises.map(item => {
+        return {
+            params: { name: item.name }
+        }
+    });
+
+    return {
+        paths,
+        fallback:false
+    }
+}
+
+//fetch my data
+export const getStaticProps = async (context) => {
+    const name = context.params.name;
+    const cruise = data.filter((item) => item.name === name)[0]
+
+    return {
+        props: {cruise},
+        }
+
+}
+
+
+export default Cruises;
